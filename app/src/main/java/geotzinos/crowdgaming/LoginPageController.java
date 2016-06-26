eb5e7441f5bf6c0ef0b7d659fd892e2bdb90e6e6
George Tzinos <geotzinos@gmail.com>
2016-06-26 08:30:58 +0300

    ClickEventManager definition

--- app/src/main/java/geotzinos/crowdgaming/LoginPageController.java
+++ app/src/main/java/geotzinos/crowdgaming/LoginPageController.java
@@ -3,6 +3,7 @@ package geotzinos.crowdgaming;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -28,5 +29,18 @@ public class LoginPageController extends AppCompatActivity {
         etEmail = (EditText) findViewById(R.id.emailTextField);
         etPassword = (EditText) findViewById(R.id.passwordTextField);
         loginButton = (Button) findViewById(R.id.login_button);
+    }
+
+    /**
+     * All onclick events
+     *
+     * @param v
+     */
+    public void ClickEventManager(View v) {
+        switch (v.getId()) {
+            case R.id.login_button:
+                //Login();
+                break;
+        }
     }
 }
