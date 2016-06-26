1f08e08a30ebf930530227296ec0ef0c30abd812
George Tzinos <geotzinos@gmail.com>
2016-06-26 07:59:35 +0300

    Initial commit

---
+++ app/src/main/java/geotzinos/crowdgaming/LoginPageController.java
@@ -0,0 +1,13 @@
+package geotzinos.crowdgaming;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginPageController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page_view);
+    }
+}
