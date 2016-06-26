63e9bc9f31dbba4347e05dce16f5e2ccdd308d1d
George Tzinos <geotzinos@gmail.com>
2016-06-26 08:35:59 +0300

    LoginRequest defined

---
+++ app/src/main/java/geotzinos/crowdgaming/Requests/LoginRequest.java
@@ -0,0 +1,18 @@
+package geotzinos.crowdgaming.Requests;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
+/**
+ * Created by George on 2016-05-29.
+ */
+public class LoginRequest extends JsonObjectRequest {
+    private static final String LOGIN_URL = "http://83.212.118.212/Treasure-Thess-Website/public/rest_api/authenticate";
+
+    //private JSONObject credentialsData;
+    public LoginRequest(JSONObject body, Response.Listener<JSONObject> responseListener, Response.ErrorListener errorListener) {
+        super(Method.POST, LOGIN_URL, body, responseListener, errorListener);
+    }
+}
