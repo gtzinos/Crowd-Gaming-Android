7db8e5470622f4bfd231207bf6735dc8d87a6252
George Tzinos <geotzinos@gmail.com>
2016-06-26 08:45:05 +0300

    User model class

---
+++ app/src/main/java/geotzinos/crowdgaming/Model/User.java
@@ -0,0 +1,55 @@
+package geotzinos.crowdgaming.Model;
+
+/**
+ * Created by George on 2016-05-29.
+ */
+public class User {
+    private String name;
+    private String surname;
+    private String email;
+    private String apiTaken;
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getApiTaken() {
+        return apiTaken;
+    }
+
+    public void setApiTaken(String apiTaken) {
+        this.apiTaken = apiTaken;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //Default constructor
+    public User() {
+    }
+
+    public User(String name, String surname, String email, String apiTaken) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.apiTaken = apiTaken;
+    }
+}
