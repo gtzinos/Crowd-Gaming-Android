0ebc7e0e109389ea8b6aeb341f4324c932f68c33
George Tzinos <geotzinos@gmail.com>
2016-06-26 15:11:23 +0300

    Config class defined

---
+++ app/src/main/java/geotzinos/crowdgaming/General/Config.java
@@ -0,0 +1,25 @@
+package geotzinos.crowdgaming.General;
+
+/**
+ * Created by George on 2016-06-26.
+ */
+public class Config {
+
+    //Debug (Default=false) *Change to false before production
+    public static boolean Debug = false;
+
+    //Server domain name or ip address
+    public static String WEB_ROOT = "83.212.118.212";
+
+    //Application name
+    public static String SHORT_APP_NAME = "Crowd Game";
+
+    //Full application name
+    public static String FULL_APP_NAME = "Crowding Game";
+
+    //Date format for application requests
+    public static String DATE_FORMAT = "DD-MM-YYYY";
+
+    //Google api key
+    public static String GOOGLE_API_KEY = "AIzaSyCclvgjNy2vp9rD8TrAnbNs4wvXft7hKiY";
+}
