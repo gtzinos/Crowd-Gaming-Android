a9cab667393b5f360af4ff0e500937bc39929f40
George Tzinos <geotzinos@gmail.com>
2016-06-26 08:30:03 +0300

    Login page elements initialization

--- app/src/main/java/geotzinos/crowdgaming/LoginPageController.java
+++ app/src/main/java/geotzinos/crowdgaming/LoginPageController.java
@@ -1,13 +1,32 @@
 package geotzinos.crowdgaming;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginPageController extends AppCompatActivity {
+    //UI Elements
+    private EditText etEmail;
+    private EditText etPassword;
+    private Button loginButton;
+
+    //Saved values
+    private String emailText;
+    private String passwordText;
+
+    //Spinner refference
+    private ProgressDialog spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page_view);
+
+        //Initialize refferences
+        etEmail = (EditText) findViewById(R.id.emailTextField);
+        etPassword = (EditText) findViewById(R.id.passwordTextField);
+        loginButton = (Button) findViewById(R.id.login_button);
     }
 }
