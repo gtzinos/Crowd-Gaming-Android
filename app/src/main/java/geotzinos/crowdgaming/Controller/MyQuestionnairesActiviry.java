d5c3aac7cb19e29dd6fcc3654c4736109d14ae7b
George Tzinos <geotzinos@gmail.com>
2016-06-29 20:56:52 +0300

    Get my questionnaires on my questionnaires activity

--- app/src/main/java/geotzinos/crowdgaming/Controller/MyQuestionnairesActiviry.java
+++ app/src/main/java/geotzinos/crowdgaming/Controller/MyQuestionnairesActiviry.java
@@ -2,13 +2,17 @@ package geotzinos.crowdgaming.Controller;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.util.ArrayList;
+
 import geotzinos.crowdgaming.General.Effect;
+import geotzinos.crowdgaming.Model.Domain.Questionnaire;
 import geotzinos.crowdgaming.Model.Domain.User;
 import geotzinos.crowdgaming.R;
 import geotzinos.crowdgaming.Request.MyQuestionnairesPageRequest;
@@ -31,5 +35,32 @@ public class MyQuestionnairesActiviry extends AppCompatActivity {
         JsonObjectRequest request = new MyQuestionnairesPageRequest().GetQuestionnaires(this, user);
         RequestQueue mRequestQueue = Volley.newRequestQueue(this);
         mRequestQueue.add(request);
+
+        ArrayList<Questionnaire> questionnaires;
+        boolean found = false;
+
+        while (found) {
+            Bundle extras = intent.getExtras();
+            if (extras != null) {
+                if (extras.containsKey("questionnaires")) {
+                    questionnaires = (ArrayList<Questionnaire>) intent.getSerializableExtra("questionnaires");
+                    break;
+                } else {
+                    // Execute some code after 2 seconds have passed
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Effect.Log("Class MyQuestionnairesActivity", "Wait for request.");
+                        }
+                    }, 100);
+                }
+            }
+
+        }
+
+
+
+
     }
 }
