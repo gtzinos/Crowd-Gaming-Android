625f6cb180855bacba5e8f58ff4ef394553f0ce9
George Tzinos <geotzinos@gmail.com>
2016-06-30 20:52:35 +0300

    I defined insertQuestionnaire , getQuestionnaires from database on QuestionnaireMapper class

--- app/src/main/java/geotzinos/crowdgaming/Model/Mapper/QuestionnaireMapper.java
+++ app/src/main/java/geotzinos/crowdgaming/Model/Mapper/QuestionnaireMapper.java
@@ -1,7 +1,65 @@
 package geotzinos.crowdgaming.Model.Mapper;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+
+import java.util.ArrayList;
+
+import geotzinos.crowdgaming.Core.Database.Database;
+import geotzinos.crowdgaming.Model.Domain.Questionnaire;
+
 /**
  * Created by George on 2016-06-29.
  */
 public class QuestionnaireMapper {
+    private Database database;
+
+    public QuestionnaireMapper(Database database) {
+        this.database = database;
+    }
+
+    /*
+        Insert questionnaire into database
+    */
+    public boolean insertQuestionnaire(Questionnaire questionnaire) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", questionnaire.getName());
+        contentValues.put("description", questionnaire.getDescription());
+        contentValues.put("creation_date", questionnaire.getCreationDate());
+        contentValues.put("time_left", questionnaire.getTime_left());
+        contentValues.put("time_left_to_end", questionnaire.getTime_left_to_end());
+        contentValues.put("total_questions", questionnaire.getTotal_questions());
+        contentValues.put("answered_questions", questionnaire.getAnswered_questions());
+        contentValues.put("allow_multiple_groups_play_through", questionnaire.getAllow_multiple_groups_playthrough());
+
+        database.getWritableDatabase().insert("questionnaires", null, contentValues);
+        return true;
+    }
+
+    /*
+        Get questionnaires from database
+    */
+    public ArrayList<Questionnaire> getQuestionnaires() {
+
+        Cursor cursor = database.getWritableDatabase().rawQuery("Select * from questionnaires", null);
+        ArrayList<Questionnaire> questionnairesList = new ArrayList<Questionnaire>();
+
+        while (cursor.moveToNext()) {
+            String name = cursor.getString(0);
+            String description = cursor.getString(1);
+            String creation_date = cursor.getString(2);
+            int time_left = cursor.getInt(3);
+            int time_left_to_end = cursor.getInt(4);
+            int total_questions = cursor.getInt(5);
+            int answered_questions = cursor.getInt(6);
+            int allow_multiple_groups_play_through = cursor.getInt(7);
+
+            Questionnaire questionnaire = new Questionnaire(name, description, creation_date, time_left, time_left_to_end, total_questions, answered_questions, allow_multiple_groups_play_through);
+            questionnairesList.add(questionnaire);
+        }
+
+        return questionnairesList;
+    }
 }
+
+
