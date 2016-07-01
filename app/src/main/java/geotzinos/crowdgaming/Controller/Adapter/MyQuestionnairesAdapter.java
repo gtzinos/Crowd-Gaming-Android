3b325643c4c7fb1c76ce278b71bd6502134c1f3f
George Tzinos <geotzinos@gmail.com>
2016-07-01 20:30:24 +0300

    CountDown timer on MyQuestionnaires activity

--- app/src/main/java/geotzinos/crowdgaming/Controller/Adapter/MyQuestionnairesAdapter.java
+++ app/src/main/java/geotzinos/crowdgaming/Controller/Adapter/MyQuestionnairesAdapter.java
@@ -1,6 +1,7 @@
 package geotzinos.crowdgaming.Controller.Adapter;
 
 import android.content.Context;
+import android.os.CountDownTimer;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -51,8 +52,8 @@ public class MyQuestionnairesAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        Holder holder = new Holder();
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        final Holder holder = new Holder();
         View rowView;
         rowView = inflater.inflate(R.layout.my_questionnaires_adapter_view, null);
         holder.timeLeftTextView = (TextView) rowView.findViewById(R.id.TimeTextView);
@@ -60,7 +61,62 @@ public class MyQuestionnairesAdapter extends BaseAdapter {
         holder.questionnaireDescriptionTextView = (TextView) rowView.findViewById(R.id.DescriptionTextView);
         holder.playQuestionnaireButton = (Button) rowView.findViewById(R.id.PlayButton);
 
-        holder.timeLeftTextView.setText("" + questionnaires.get(position).getTime_left());
+        if (questionnaires.get(position).getTime_left() == 0) {
+            if (questionnaires.get(position).getAnswered_questions() == questionnaires.get(position).getAnswered_questions()) {
+                holder.playQuestionnaireButton.setFocusable(false);
+                holder.playQuestionnaireButton.setClickable(false);
+                holder.playQuestionnaireButton.setActivated(false);
+                holder.timeLeftTextView.setText(Html.fromHtml("<div><font color='#5cb85c'>Completed</font></div>"));
+            } else {
+                holder.playQuestionnaireButton.setClickable(true);
+                holder.playQuestionnaireButton.setActivated(true);
+                holder.timeLeftTextView.setText(Html.fromHtml("<div><font color='#5cb85c'>Running</font></div>"));
+            }
+        } else if (questionnaires.get(position).getTime_left() == -1) {
+            holder.playQuestionnaireButton.setFocusable(false);
+            holder.playQuestionnaireButton.setClickable(false);
+            holder.playQuestionnaireButton.setActivated(false);
+            holder.timeLeftTextView.setText(Html.fromHtml("<div><font color='#f0ad4e'>Not scheduled yet.</font></div>"));
+        } else {
+            long milliseconds = questionnaires.get(position).getTime_left() * 60000;
+            CountDownTimer timer = new CountDownTimer(milliseconds, 100) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    long days = 0, hours = 0, minutes = 0, seconds = 0;
+                    String timerValue = "";
+
+                    seconds = millisUntilFinished / 1000;
+
+                    days = seconds / 86400;
+                    if (days > 0) {
+                        seconds = seconds % 86400;
+                        timerValue += days + "d ";
+                    }
+
+                    hours = seconds / 3600;
+                    if (hours > 0) {
+                        seconds = seconds % 3600;
+                    }
+                    timerValue += hours + ":";
+
+                    minutes = seconds / 60;
+                    if (minutes > 0) {
+                        seconds = seconds % 60;
+                    }
+                    timerValue += minutes + ":";
+
+                    timerValue += seconds;
+
+                    holder.timeLeftTextView.setText(Html.fromHtml("<div><font color='#d9534f'>" + timerValue + "</font></div>"));
+                }
+
+                @Override
+                public void onFinish() {
+                    holder.playQuestionnaireButton.setEnabled(true);
+                }
+            }.start();
+        }
+
         holder.questionnaireNameTextView.setText(questionnaires.get(position).getName());
         holder.questionnaireDescriptionTextView.setText(Html.fromHtml(questionnaires.get(position).getDescription()));
         return rowView;
