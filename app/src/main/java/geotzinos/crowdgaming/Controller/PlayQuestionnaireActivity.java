55874fa3860f93e2f8403c0cc6af46bc3d7188f1
George Tzinos <geotzinos@gmail.com>
2016-07-02 19:03:39 +0300

    Get user in PlayQuestionnaireActivity, GetQuestionGroup request definition.

--- app/src/main/java/geotzinos/crowdgaming/Controller/PlayQuestionnaireActivity.java
+++ app/src/main/java/geotzinos/crowdgaming/Controller/PlayQuestionnaireActivity.java
@@ -6,7 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import geotzinos.crowdgaming.Controller.Request.PlayQuestionnairePageRequest;
 import geotzinos.crowdgaming.Model.Domain.Questionnaire;
+import geotzinos.crowdgaming.Model.Domain.User;
 import geotzinos.crowdgaming.R;
 
 public class PlayQuestionnaireActivity extends AppCompatActivity {
@@ -24,9 +30,10 @@ public class PlayQuestionnaireActivity extends AppCompatActivity {
         //Get extra values
         Intent intent = getIntent();
         Questionnaire questionnaire = (Questionnaire) intent.getSerializableExtra("questionnaire");
+        User user = (User) intent.getSerializableExtra("user");
         //Send request to get groups
-        // JsonObjectRequest request = new MyQuestionnairesPageRequest().GetQuestionnaires(this, questionnaire, listView);
-        // RequestQueue mRequestQueue = Volley.newRequestQueue(this);
-        // mRequestQueue.add(request);
+        JsonObjectRequest request = new PlayQuestionnairePageRequest().GetQuestionGroups(this, user, questionnaire, listView);
+        RequestQueue mRequestQueue = Volley.newRequestQueue(this);
+        mRequestQueue.add(request);
     }
 }
