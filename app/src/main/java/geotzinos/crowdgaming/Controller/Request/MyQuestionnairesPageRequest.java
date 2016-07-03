ddb1ead39ec81800fd46b2c4e079c170bc1f42c8
George Tzinos <geotzinos@gmail.com>
2016-07-03 20:14:02 +0300

    GetQuestionGroups request moved from PlayQuestionnairePageRequest to MyQuestionnairesPageRequest

--- app/src/main/java/geotzinos/crowdgaming/Controller/Request/MyQuestionnairesPageRequest.java
+++ app/src/main/java/geotzinos/crowdgaming/Controller/Request/MyQuestionnairesPageRequest.java
@@ -1,6 +1,7 @@
 package geotzinos.crowdgaming.Controller.Request;
 
 import android.content.Context;
+import android.content.Intent;
 import android.widget.ListView;
 
 import com.android.volley.AuthFailureError;
@@ -17,8 +18,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 import geotzinos.crowdgaming.Controller.Adapter.MyQuestionnairesAdapter;
+import geotzinos.crowdgaming.Controller.PlayQuestionnaireActivity;
 import geotzinos.crowdgaming.General.Config;
 import geotzinos.crowdgaming.General.Effect;
+import geotzinos.crowdgaming.Model.Domain.QuestionGroup;
 import geotzinos.crowdgaming.Model.Domain.Questionnaire;
 import geotzinos.crowdgaming.Model.Domain.User;
 
@@ -69,6 +72,73 @@ public class MyQuestionnairesPageRequest {
                 Effect.Log("Class MyQuestionnairesPageRequest", error.getMessage());
                 Effect.CloseSpinner();
                 Effect.Alert(context, "Wrong username or password.", "Okay");
+            }
+
+        }) {
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Authorization", user.getApiTaken());
+
+                return headers;
+            }
+        };
+
+        return request;
+    }
+
+    public JsonObjectRequest GetQuestionGroups(final Context context, final User user, final Questionnaire questionnaire) {
+        final String URL = Config.WEB_ROOT + "/rest_api/questionnaire/" + questionnaire.getId() + "/group";
+
+        JsonObjectRequest request = new JsonObjectRequest(URL, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            int code = response.getInt("code");
+
+                            if (code == 200) {
+                                JSONArray groupsJArray = response.getJSONArray("question-group");
+                                for (int i = 0; i < groupsJArray.length(); i++) {
+                                    JSONObject questionGroupJObject = groupsJArray.getJSONObject(i);
+
+                                    long id = questionGroupJObject.getLong("id");
+                                    String name = questionGroupJObject.getString("name");
+                                    String latitude = questionGroupJObject.getString("latitude");
+                                    String longitude = questionGroupJObject.getString("longitude");
+                                    String radius = questionGroupJObject.getString("radius");
+                                    String creation_date = questionGroupJObject.getString("creation_date");
+                                    long total_questions = questionGroupJObject.getLong("total-questions");
+                                    long answered_questions = questionGroupJObject.getLong("answered-questions");
+                                    long allowed_repeats = questionGroupJObject.getLong("allowed-repeats");
+                                    long current_repeats = questionGroupJObject.getLong("current-repeats");
+                                    String time_left = questionGroupJObject.getString("time-left");
+                                    String time_to_complete = questionGroupJObject.getString("time-to-complete");
+                                    long priority = questionGroupJObject.getLong("priority");
+                                    String is_completed = questionGroupJObject.getString("is-completed");
+
+                                    QuestionGroup questionGroup = new QuestionGroup(id, name, latitude, longitude, radius, creation_date, total_questions, answered_questions, allowed_repeats, current_repeats, time_left, time_to_complete, priority, is_completed);
+                                    questionnaire.getQuestionGroupsList().add(questionGroup);
+                                }
+
+                                Intent intent = new Intent(context, PlayQuestionnaireActivity.class);
+                                intent.putExtra("questionnaire", questionnaire);
+                                intent.putExtra("user", user);
+                                context.startActivity(intent);
+                                Effect.Log("Class MyQuestionnairesPageRequest", "Get question groups request completed.");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Effect.Log("Class MyQuestionnairesPageRequest", error.getMessage());
+                Effect.CloseSpinner();
+                Effect.Alert(context, "You can't play this questionnaire.", "Okay");
             }
 
         }) {
