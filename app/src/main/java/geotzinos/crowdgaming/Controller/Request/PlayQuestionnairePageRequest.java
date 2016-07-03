93af2c82a60577372cca6c0adf03b0ae9f063b1f
George Tzinos <geotzinos@gmail.com>
2016-07-03 12:51:26 +0300

    GetNextQuestion request defined successfully.

--- app/src/main/java/geotzinos/crowdgaming/Controller/Request/PlayQuestionnairePageRequest.java
+++ app/src/main/java/geotzinos/crowdgaming/Controller/Request/PlayQuestionnairePageRequest.java
@@ -2,6 +2,7 @@ package geotzinos.crowdgaming.Controller.Request;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -15,12 +16,16 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import geotzinos.crowdgaming.Controller.Adapter.PlayQuestionnairesAdapter;
+import geotzinos.crowdgaming.Controller.AnswerQuestionGroupActivity;
 import geotzinos.crowdgaming.General.Config;
 import geotzinos.crowdgaming.General.Effect;
+import geotzinos.crowdgaming.Model.Domain.Answer;
+import geotzinos.crowdgaming.Model.Domain.Question;
 import geotzinos.crowdgaming.Model.Domain.QuestionGroup;
 import geotzinos.crowdgaming.Model.Domain.Questionnaire;
 import geotzinos.crowdgaming.Model.Domain.User;
@@ -139,6 +144,67 @@ public class PlayQuestionnairePageRequest {
                 User user = (User) ((Activity) context).getIntent().getSerializableExtra("user");
                 headers.put("Authorization", user.getApiTaken());
 
+                return headers;
+            }
+        };
+
+        return request;
+    }
+
+    public JsonObjectRequest GetNextQuestion(final Context context, final User user, final String questionnaire_id, final String group_id) {
+        final String URL = Config.WEB_ROOT + "/rest_api/questionnaire/" + questionnaire_id + "/group/" + group_id + "/question";
+        Effect.ShowSpinner(context);
+        JsonObjectRequest request = new JsonObjectRequest(URL, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            int code = response.getInt("code");
+                            Effect.CloseSpinner();
+                            if (code == 200) {
+                                JSONObject questionJObject = response.getJSONObject("question");
+                                long id = questionJObject.getLong("id");
+                                String name = questionJObject.getString("question-text");
+                                double multiplier = questionJObject.getDouble("multiplier");
+                                String creation_date = questionJObject.getString("creation_date");
+                                double time_to_answer = questionJObject.getDouble("time-to-answer");
+                                Question question = new Question(id, name, multiplier, creation_date);
+
+                                JSONArray answerJArray = response.getJSONArray("answer");
+                                ArrayList<Answer> answersArrayList = new ArrayList<Answer>();
+                                for (int i = 0; i < answerJArray.length(); i++) {
+                                    JSONObject answerJObject = answerJArray.getJSONObject(i);
+                                    long answer_id = answerJObject.getLong("id");
+                                    String answer_name = answerJObject.getString("answer-text");
+                                    String answer_creation_date = answerJObject.getString("creation_date");
+                                    Answer answer = new Answer(answer_id, answer_name, answer_creation_date);
+                                    question.getAnswersList().add(answer);
+                                }
+                                Intent intent = new Intent(context, AnswerQuestionGroupActivity.class);
+                                intent.putExtra("user", user);
+                                intent.putExtra("question", question);
+                                context.startActivity(intent);
+                                ((Activity) context).finish();
+                                Effect.Log("Class PlayQuestionnairePageRequest", "Get question request completed.");
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Effect.Log("Class PlayQuestionnairePageRequest", error.getMessage());
+                Effect.CloseSpinner();
+            }
+
+        }) {
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Authorization", user.getApiTaken());
+                //TODO X-Coordinates
                 return headers;
             }
         };
