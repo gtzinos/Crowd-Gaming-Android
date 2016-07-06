4385a7563bec282328efd1e00f4cb4c794df94a0
George Tzinos <geotzinos@gmail.com>
2016-07-06 07:51:54 +0300

    Set adapter to answers listview

--- app/src/main/java/geotzinos/crowdgaming/Controller/AnswerQuestionGroupActivity.java
+++ app/src/main/java/geotzinos/crowdgaming/Controller/AnswerQuestionGroupActivity.java
@@ -5,11 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-import geotzinos.crowdgaming.Controller.Request.MyQuestionnairesPageRequest;
+import geotzinos.crowdgaming.Controller.Adapter.AnswerQuestionGroupAdapter;
+import geotzinos.crowdgaming.Model.Domain.Question;
 import geotzinos.crowdgaming.Model.Domain.User;
 import geotzinos.crowdgaming.R;
 
@@ -20,12 +17,15 @@ public class AnswerQuestionGroupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.answer_question_group_view);
+        listView = (ListView) findViewById(R.id.AnswersListView);
 
         Intent intent = getIntent();
         User user = (User) intent.getSerializableExtra("user");
-        listView = (ListView) findViewById(R.id.AnswersListView);
-        JsonObjectRequest request = new MyQuestionnairesPageRequest().GetQuestionnaires(this, user, listView);
-        RequestQueue mRequestQueue = Volley.newRequestQueue(this);
-        mRequestQueue.add(request);
+        Question question = (Question) intent.getSerializableExtra("question");
+        listView.setAdapter(new AnswerQuestionGroupAdapter(this, question));
+
+        //JsonObjectRequest request = new MyQuestionnairesPageRequest().GetQuestionnaires(this, user, listView);
+        //RequestQueue mRequestQueue = Volley.newRequestQueue(this);
+        //mRequestQueue.add(request);
     }
 }
