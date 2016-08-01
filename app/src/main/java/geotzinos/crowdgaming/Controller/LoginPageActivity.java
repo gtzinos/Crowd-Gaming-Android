9713c5bb8f0f0ff2c8aa5cd68dbaa1e2c31c8fec
George Tzinos <geotzinos@gmail.com>
2016-08-01 23:43:23 +0300

    {NEW} Auto login without to send again the credentials.

--- app/src/main/java/geotzinos/crowdgaming/Controller/LoginPageActivity.java
+++ app/src/main/java/geotzinos/crowdgaming/Controller/LoginPageActivity.java
@@ -1,5 +1,9 @@
 package geotzinos.crowdgaming.Controller;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -15,10 +19,18 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 import geotzinos.crowdgaming.Controller.Request.LoginPageRequest;
 import geotzinos.crowdgaming.General.Config;
 import geotzinos.crowdgaming.General.Effect;
 import geotzinos.crowdgaming.General.Validate;
+import geotzinos.crowdgaming.Model.Domain.User;
 import geotzinos.crowdgaming.R;
 
 public class LoginPageActivity extends AppCompatActivity {
@@ -32,6 +44,43 @@ public class LoginPageActivity extends AppCompatActivity {
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page_view);
+
+        User user = new User();
+        try {
+            InputStream inputStream = openFileInput("user.txt");
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                if(stringBuilder.length() > 0) {
+                    String data[] = stringBuilder.toString().split("###");
+
+                    user.setName(data[0].replace("name=",""));
+                    user.setSurname(data[1].replace("surname=",""));
+                    user.setApiTaken(data[2].replace("token=",""));
+                    if(user.getApiTaken() != null)
+                    {
+                        Intent intent = new Intent(this, MyQuestionnairesActiviry.class);
+                        intent.putExtra("user", user);
+
+                        startActivity(intent);
+                    }
+                }
+            }
+        }
+        catch (FileNotFoundException e) {
+            Effect.Log("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Effect.Log("login activity", "Can not read file: " + e.toString());
+        }
 
         //Initialize refferences
         etEmail = (EditText) findViewById(R.id.emailTextField);
