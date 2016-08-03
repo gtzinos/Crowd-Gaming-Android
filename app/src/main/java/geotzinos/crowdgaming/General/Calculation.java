de9e6cba3c1db753b148b17336f8c145514ab4ae
George Tzinos <geotzinos@gmail.com>
2016-08-03 07:30:23 +0300

    {FIX} I moved calculateDistance to Calculation class

--- app/src/main/java/geotzinos/crowdgaming/General/Calculation.java
+++ app/src/main/java/geotzinos/crowdgaming/General/Calculation.java
@@ -1,7 +1,16 @@
 package geotzinos.crowdgaming.General;
 
+import android.location.Location;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
+import geotzinos.crowdgaming.Model.Domain.QuestionGroup;
 
 /**
  * Created by George on 2016-07-03.
@@ -45,5 +54,39 @@ public class Calculation {
             e.printStackTrace();
         }
         return value;
+    }
+
+    //calculate client distance from question group
+    public static String calculateDistance(QuestionGroup questionGroup, Location location) {
+        long R = 6371; // Radius of the earth in km
+        double dLat = Math.toRadians(Double.parseDouble(questionGroup.getLatitude()) - location.getLatitude());
+        double dLon = Math.toRadians(Double.parseDouble(questionGroup.getLongitude()) - location.getLongitude());
+        double lat1 = Math.toRadians(Double.parseDouble(questionGroup.getLatitude()));
+        double lat2 = Math.toRadians(location.getLatitude());
+
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double d = R * c;
+
+        //Convert km to meters
+        d = d * 1000;
+
+        //Check the final distance
+        if (d - Double.parseDouble(questionGroup.getRadius()) >= 0) {
+            d = d - Double.parseDouble(questionGroup.getRadius());
+        } else {
+            d = 0;
+        }
+        DecimalFormatSymbols otherSymbols = new DecimalFormatSymbols(Locale.getDefault());
+        otherSymbols.setDecimalSeparator('.');
+        otherSymbols.setGroupingSeparator(',');
+        DecimalFormat df = new DecimalFormat("#.##", otherSymbols);
+        df.setRoundingMode(RoundingMode.CEILING);
+
+        df.format(d);
+
+        //return value formatted with 2 decimals
+        return df.format(d);
     }
 }
