f01c579b0b34afdc9f2b2dd08735b61fe0d22d9b
George Tzinos <geotzinos@gmail.com>
2016-08-07 09:00:37 +0300

    {FIX} Return a positive code to operating system when then user close his application from LoginPageActivity.

--- app/src/main/java/geotzinos/crowdgaming/Controller/LoginPageActivity.java
+++ app/src/main/java/geotzinos/crowdgaming/Controller/LoginPageActivity.java
@@ -159,7 +159,7 @@ public class LoginPageActivity extends AppCompatActivity {
                     public void onClick(DialogInterface dialog, int which) {
                         ((Activity)context).finish();
                         android.os.Process.killProcess(android.os.Process.myPid());
-                        System.exit(1);
+                        System.exit(0);
                     }
                 });
                 exit_alert.setNegativeButton("Cancel",null);
